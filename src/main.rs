cb9d05e51a00780e84109b505bc8d5e3f31bd5c7
valentynbez <walentyn.bzsz@gmail.com>
2023-11-02 11:27:08 +0100

    fix: skip duplicated sequence

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,15 @@ use std::io::{self, BufRead};
 use std::fs::File;
 use std::path::Path;
 use std::env;
+use std::collections::HashMap;
+
+
+struct Sequence {
+    header: String,
+    seq: String,
+    kmers: HashMap<String, usize>
+}
+
 
 
 fn parse_fasta (filename: &str) -> (Vec<String>, Vec<String>) {
@@ -17,18 +26,21 @@ fn parse_fasta (filename: &str) -> (Vec<String>, Vec<String>) {
     let reader: io::BufReader<File> = io::BufReader::new(file);
     let mut headers: Vec<String> = Vec::new();
     let mut seq: Vec<String> = Vec::new();
+    let mut skip_entry: bool = false;
     for line in reader.lines() {
         let line: String = line.unwrap();
         if line.starts_with('>') {
             let header: String = line[1..].to_string();
             if headers.contains(&header) {
                 println!("Warning: duplicate header {} in file {}. Skipping...", line, filename);
+                skip_entry = true;
             }
             else {
                 headers.push(header);
+                skip_entry = false;
             }
         }
-        else {
+        else if !skip_entry{
             seq.push(line);
         }
     }
@@ -67,27 +79,31 @@ fn main () {
 
     // read FASTA file
     let (headers, seq): (Vec<String>, Vec<String>) = parse_fasta(filename);
+    for s in &seq {
+        println!("{}", s);
+    }
 
-    // iterate over sequence and count kmers
-    let mut kmer: Vec<String> = Vec::new();
-    let mut count: Vec<usize> = Vec::new();
+    // iterate over sequence and count kmers in each row
+    let mut seqs_kmers: Vec<Sequence> = Vec::new();
     for i in 0..seq.len() {
+        let mut kmer_count = HashMap::new();
         let (kmer_tmp, count_tmp): (Vec<String>, Vec<usize>) = count_kmers(&seq[i], *k);
         for j in 0..kmer_tmp.len() {
-            if kmer.contains(&kmer_tmp[j]) {
-                let index: usize = kmer.iter().position(|x| *x == kmer_tmp[j]).unwrap();
-                count[index] += count_tmp[j];
-            }
-            else {
-                kmer.push(kmer_tmp[j].clone());
-                count.push(count_tmp[j]);
-            }
+            kmer_count.insert(kmer_tmp[j].clone(), count_tmp[j]);
         }
-    }
+        let seq_tmp: Sequence = Sequence {
+            header: headers[i].clone(),
+            seq: seq[i].clone(),
+            kmers: kmer_count
+        };
 
-    // print k-mers
-    for i in 0..kmer.len() {
-        println!("{}\t{}", kmer[i], count[i]);
-    }
+        seqs_kmers.push(seq_tmp);
 
+        // println!("Sequence: {}", headers[i]);
+        // println!("K-mer\tCount");
+        // for j in 0..kmer_tmp.len() {
+        //     println!("{}\t{}", kmer_tmp[j], count_tmp[j]);
+        // }
+
+    }
 }
